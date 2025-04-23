29858e30c890304ab24aedbf804d8cbcbaf8a2ab
Hans Chen <hanschen31310@gmail.com>
2025-04-23 14:58:04 +0800

    04/23,流程完善,方法等待debug,視窗顯示整合

--- DIP_Assignment3.py
+++ DIP_Assignment3.py
@@ -15,35 +15,44 @@ def FourierTransform(image):
 
     return magnitude_spectrum, phase_spectrum
 
-def createWindow(image, title = 'Image'): # 創建一個圖形窗口並顯示原圖
-    plt.figure()
-    plt.imshow(image, cmap='gray')
-    plt.title(title)
-    plt.xticks([]), plt.yticks([])
-
-def gaussianFilter(image, sigma = 10, img_notch = None):
-    # 設置高斯濾波器
-    rows, cols = image.shape
-    crow, ccol = rows // 2, cols // 2
-    sigma = 30
-    x = np.linspace(-ccol, ccol, cols)
-    y = np.linspace(-crow, crow, rows)
-    x, y = np.meshgrid(x, y)
-    mask = np.exp(-(x**2 + y**2) / (2 * sigma**2)) # 高斯函數
-    mask = mask / mask.max()
-    mask = np.repeat(mask[:, :, np.newaxis], 2, axis = 2)  # 重複以匹配通道數
+def createWindow(image = None, title = None): # 創建一個圖形視窗
+    plt.figure() # 創建一個圖形視窗
     
-    return mask
+    if image is not None:
+        plt.imshow(image, cmap='gray')
 
-    # filtered_image = image * mask
+    if title is not None:
+        plt.title(title)
+    else:
+        # 去除圖形標框
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['left'].set_visible(False)
+        plt.gca().spines['bottom'].set_visible(False)
+        
+    plt.xticks([]), plt.yticks([]) # 隱藏坐標軸刻度
 
-    # return filtered_image
+def ideaFilter(img, sigma = 10, img_notch = None):
+    img_filtered = img.copy()
+    
+    for point in img_notch:
+        cv2.circle(img_filtered, tuple(point), radius = 5, color = 0, thickness = -1)
+
+    return img_filtered
+
+def alterFourierTransform(image):
+    f_ishift = np.fft.ifftshift(image)
+    img_processed = np.fft.ifft2(f_ishift)
+    img_processed = np.abs(img_processed)
+    
+    return img_processed
 
 if __name__ == "__main__":
     
     img = cv2.imread('input_image/image1.jpg', 0)  # 以灰度模式讀取圖像
     
     row, col = img.shape
+    
     img_point = [[321, 109], 
                  [238, 171], [404, 171], 
                  [155, 236], [486, 236], 
@@ -51,14 +60,22 @@ if __name__ == "__main__":
                  [321, 359]]
 
     magnitude_spectrum, phase_spectrum = FourierTransform(img)
-    # filtered_image = gaussianFilter(magnitude_spectrum, sigma=10, img_notch = img_point)
+    magnitude_spectrum_filtered = ideaFilter(magnitude_spectrum, sigma=10, img_notch = img_point)
+    image_filtered = alterFourierTransform(magnitude_spectrum_filtered) # 進行反傅立葉轉換
+    magnitude_spectrum_filtered, phase_spectrum_filtered = FourierTransform(image_filtered) # 重新計算濾波後的幅度譜和相位頻譜
     
-    createWindow(img)  # 顯示原圖
-    createWindow(magnitude_spectrum, "Magnitude Spectrum")  # 顯示幅度譜
-    createWindow(phase_spectrum, "Phase Spectrum")  # 顯示相位頻譜
-    # createWindow(filtered_image, "Filtered Image")  # 顯示濾波後的圖像
-
+    # createWindow(img)  # 顯示原圖
+    # createWindow(magnitude_spectrum, "Magnitude Spectrum")  # 顯示原幅度譜
+    # createWindow(phase_spectrum, "Phase Spectrum")  # 顯示原相位頻譜
+    createWindow(magnitude_spectrum_filtered, "Filtered Magnitude Spectrum")  # 顯示濾波後的幅度譜
+    createWindow(image_filtered, "Filtered Image")  # 顯示濾波後的圖像
+    
+    createWindow()
+    plt.subplot(131), plt.imshow(img, cmap='gray'), plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(132).imshow(magnitude_spectrum, cmap='gray'), plt.title("Magnitude Spectrum"), plt.xticks([]), plt.yticks([])
+    plt.subplot(133).imshow(phase_spectrum, cmap='gray'), plt.title("Phase Spectrum"), plt.xticks([]), plt.yticks([])
+    
     # 顯示所有圖形窗口
     plt.show()
     
-    print("mask:{}".format(gaussianFilter(magnitude_spectrum, sigma=10, img_notch = img_point)))
+    # print("mask:{}".format(gaussianFilter(magnitude_spectrum, sigma=10, img_notch = img_point)))
